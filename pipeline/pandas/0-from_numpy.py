94acc50d9a144f9bb6e3d563f5c6d614da90bf3d
Ahmad <asusahmad26@gmail.com>
2026-01-10 18:40:39 +0400

    1

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-
 import pandas as pd
 
 def from_numpy(array):
     ndf = pd.DataFrame(array)
+    ndf.sort_index(axis=1)
+    ndf.columns = ndf.columns.str.upper()
     return ndf
