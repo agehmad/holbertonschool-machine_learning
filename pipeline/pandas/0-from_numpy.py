42d63a8f5346fb7aa0777c7cc6802ca9f4b49878
Ahmad <asusahmad26@gmail.com>
2026-01-10 18:55:45 +0400

    1

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 import pandas as pd
-import string
 
 def from_numpy(array):
     num_columns = array.shape[1]
-    columns = list(string.ascii_uppercase[:num_columns])
+    columns = [chr(ord('A') + i) for i in range(num_columns)]
     df = pd.DataFrame(array, columns=columns)
     return df
