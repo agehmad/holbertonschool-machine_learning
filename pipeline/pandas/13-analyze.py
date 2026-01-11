1bf5065d839bdd53f73a72bd5e9ad74a9cff5ce9
Ahmad <asusahmad26@gmail.com>
2026-01-11 19:29:30 +0400

    1

---
+++ pipeline/pandas/13-analyze.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Docstring for pipeline.pandas.13-analyze
+"""
+def analyze(df):
+    """
+    Docstring for analyze
+
+    :param df: Description
+    """
+    df = df.describe()
+    return df
