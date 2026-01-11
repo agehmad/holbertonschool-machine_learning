8f4c1a1f331980f3c62b6d7d2a28a23d2e838cfd
Ahmad <asusahmad26@gmail.com>
2026-01-11 13:38:31 +0400

    1

--- pipeline/pandas/6-flip_switch.py
+++ pipeline/pandas/6-flip_switch.py
@@ -5,6 +5,6 @@ Docstring for pipeline.pandas.6-flip_switch
 
 
 def flip_switch(df):
-    df = df.sort_values(ascending=False)
+    df = df.sort_values(by='column_name', ascending=False)
     df = df.transpose()
     return df
