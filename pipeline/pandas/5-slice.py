3290be6e0616b64900a7d6e6d56ec4fa62a01081
Ahmad <asusahmad26@gmail.com>
2026-01-11 13:28:14 +0400

    1

--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -5,6 +5,11 @@ Docstring for pipeline.5-slice
 
 
 def slice(df):
+    """
+    Docstring for slice
+
+    :param df: Description
+    """
     df = df[['High', 'Low', 'Close', 'Volume_(BTC)']]
     df = df.iloc[::60]
     return df
