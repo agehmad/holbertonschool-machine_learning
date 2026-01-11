9369694a855a66429990be4dd30e6eb29a1bdf79
Ahmad <asusahmad26@gmail.com>
2026-01-11 18:36:23 +0400

    1

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -8,16 +8,23 @@ import pandas as pd
 
 def concat(df1, df2):
     """
-    Docstring for concat
+    Concatenates two DataFrames with specific conditions. 
 
-    :param df1: Description
-    :param df2: Description
+    : param df1: pd.DataFrame (coinbase)
+    :param df2: pd.DataFrame (bitstamp)
+    :return: concatenated pd.DataFrame
     """
+    # Import the index function from 10-index.py
+    index = __import__('10-index').index
+
+    # Index both dataframes on their Timestamp columns
     df1 = index(df1)
     df2 = index(df2)
 
+    # Filter df2: include all timestamps up to and including 1417411920
     df2 = df2.loc[: 1417411920]
 
+    # Concatenate:  df2 on top of df1, with keys
     result = df1.__class__. concat(
         [df2, df1],
         keys=['bitstamp', 'coinbase']
