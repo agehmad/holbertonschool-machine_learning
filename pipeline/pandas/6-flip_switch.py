52cb8ecc9b383774f0cf959695811db537384d47
Ahmad <asusahmad26@gmail.com>
2026-01-11 13:43:36 +0400

    1

--- pipeline/pandas/6-flip_switch.py
+++ pipeline/pandas/6-flip_switch.py
@@ -7,7 +7,7 @@ Docstring for pipeline.pandas.6-flip_switch
 def flip_switch(df):
     """
     Docstring for flip_switch
-    
+
     :param df: Description
     """
     df = df.sort_index(ascending=False)
