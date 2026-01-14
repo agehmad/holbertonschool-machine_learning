2bbdb3ad56f5556cff3768b9a89f016835f79196
Ahmad <asusahmad26@gmail.com>
2026-01-14 15:45:15 +0400

    1

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
 """
@@ -14,3 +14,4 @@ def line():
     plt.figure(figsize=(6.4, 4.8))
     x = np.arange(0, 11)
     plt.plot(x, y, color='red')
+    plt.xlim(0, 10)
