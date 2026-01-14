716deabcc4285a75a28d19bd7bd064f1570101d8
Ahmad <asusahmad26@gmail.com>
2026-01-14 15:36:06 +0400

    1

---
+++ math/plotting/0-line.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+"""
+documentation
+"""
+
+
+def line():
+    """
+    Docstring for line
+    """
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+    x = np.arange(0, 10)
+    plt.plot(x, y)
