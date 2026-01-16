5f61ac1613c4fbf5092a67fba0a34f1c1053a6f0
Ahmad <asusahmad26@gmail.com>
2026-01-16 17:44:35 +0400

    1

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Docstring for math.calculus.9-sum_total
+"""
+
+
+def summation_i_squared(n):
+    """
+    Docstring for summation_i_squared
+
+    :param n: Description
+    """
+    nums = list(range(1, n+1))
+    if type(n) is int == False:
+        return None
+    nums = list(map(lambda x : x**2, nums))
+    return  sum(nums)
