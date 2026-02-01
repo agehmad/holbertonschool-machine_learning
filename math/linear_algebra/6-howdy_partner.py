baccf5ec27be4a04d2be9e5534c484671df62fc0
Ahmad <asusahmad26@gmail.com>
2026-02-01 14:22:33 +0400

    1

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''module documented'''
+
+
+def cat_arrays(arr1, arr2):
+    '''function documented'''
+    result = []
+    for i in arr1:
+        result.append(i)
+    for i in arr2:
+        result.append(i)
+    return result
