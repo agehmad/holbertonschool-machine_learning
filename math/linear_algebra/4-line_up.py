5730180001e56e06d955c274ca8aeaab8d0350eb
Ahmad <asusahmad26@gmail.com>
2026-02-01 14:16:04 +0400

    1

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Docstring for math.linear_algebra.4-line_up
+"""
+def add_arrays(arr1, arr2):
+    """
+    Docstring for add_arrays
+
+    :param arr1: Description
+    :param arr2: Description
+    """
+    new = []
+    if len(arr1) != len(arr2):
+        return None
+    else:
+        for i in range(len(arr1)):
+            new.append(arr1(i) + arr2(i))
+    return new
