768c99cbefa747e4a9a19e48fd13f327d9131767
Ahmad <asusahmad26@gmail.com>
2026-02-07 19:00:52 +0400

    determinant

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -5,7 +5,7 @@
 def determinant(matrix):
     """documented"""
     if (not isinstance(matrix, list) or
-         any(not isinstance(row, list) for row in matrix)):
+       any(not isinstance(row, list) for row in matrix)):
         raise TypeError("matrix must be a list of lists")
     if matrix == [[]]:
         return 1
