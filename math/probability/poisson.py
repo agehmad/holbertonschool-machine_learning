7c6eda362a3bf71e1061d374afe8a2ed1e1ca225
Ahmad <asusahmad26@gmail.com>
2026-02-11 20:56:38 +0400

    poisson

---
+++ math/probability/poisson.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+'''module documented'''
+
+
+class Poisson:
+    '''class documented'''
+    def __init__(self, data=None, lambtha=1.):
+        '''constructor documented'''
+        self.data = data
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = lambtha
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = sum(data) / len(data)
+
+    def pmf(self, k):
+        '''method1 documented'''
+        if not isinstance(k, int):
+            k = int(k)
+        if k < 0:
+            return 0
+        p = ((self.lambtha ** k) * 2.7182818285 ** (-self.lambtha)
+             / Poisson.factorial(k))
+        return p
+
+    def cdf(self, k):
+        '''method2 documented'''
+        cdf_p = 0
+        if not isinstance(k, int):
+            k = int(k)
+        if k < 0:
+            return 0
+        for i in range(0, k+1):
+            cdf_p = cdf_p + self.pmf(i)
+        return cdf_p
+
+    @staticmethod
+    def factorial(n):
+        '''method documented'''
+        p = 1
+        for i in range(1, n+1):
+            p = p * i
+        return p
