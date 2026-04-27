8e76e1234b90a92097740d0729582271e6de7996
Ahmad Ahadov <asusahmad26@gmail.com>
2026-04-27 21:19:32 +0400

    classification

---
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+'''module documented'''
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    '''class documented'''
+    def __init__(self, nx, layers):
+        '''init documented'''
+        if not (isinstance(nx, int)):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not (isinstance(layers, list)) or layers == []:
+            raise TypeError("layers must be a list of positive integers")
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+        # L = 3     1, 2
+        for layer in range(self.L):
+            if layers[layer] <= 0 or not (isinstance(layer, int)):
+                raise TypeError("layers must be a list of positive integers")
+            if layer == 0:
+                prev = nx
+            else:
+                prev = layers[layer-1]
+            W = (np.random.randn(layers[layer], prev) *
+                 np.sqrt(2 / prev))
+            b = np.zeros((layers[layer], 1))
+            self.weights.update({f"W{layer+1}": W})
+            self.weights.update({f"b{layer+1}": b})
+            # W1 : W  randn(3, 5)
+            # b1 : b zeros()
---
+++ supervised_learning/classification/8-neural_network.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''module documented'''
+import numpy as np
+
+
+class NeuralNetwork:
+    '''class documented'''
+    def __init__(self, nx, nodes):
+        '''init documented'''
+        if not (isinstance(nx, int)):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not (isinstance(nodes, int)):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.nx = nx
+        self.nodes = nodes
+        self.W1 = np.random.randn(nodes, nx)
+        #  z1 = w1*x + b
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
