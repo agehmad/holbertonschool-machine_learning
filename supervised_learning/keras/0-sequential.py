6632c368c99eb450b03105d27342e853289e56f8
Ahmad Ahadov <asusahmad26@gmail.com>
2026-05-08 20:28:53 +0400

    pycode style

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -2,6 +2,7 @@
 import tensorflow.keras as K
 """documented"""
 
+
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """documented"""
     # 784, [256, 256, 10], ['tanh', 'tanh', 'softmax'], 0.001, 0.95
@@ -10,7 +11,7 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
         if i == 0:
             model.add(K.layers.Dense(
                 layers[i],
-                input_shape = (nx,),
+                input_shape=(nx,),
                 activation=activations[i],
                 kernel_regularizer=K.regularizers.l2(l2=lambtha)
             ))
