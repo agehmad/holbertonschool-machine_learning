df53cb2d1b15a80550b2dbc64b1e32af9376cb5f
Ahmad Ahadov <asusahmad26@gmail.com>
2026-05-08 20:26:27 +0400

    1

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 import tensorflow.keras as K
+"""documented"""
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
+    """documented"""
     # 784, [256, 256, 10], ['tanh', 'tanh', 'softmax'], 0.001, 0.95
     model = K.Sequential()
     for i in range(len(layers)):
         if i == 0:
             model.add(K.layers.Dense(
+                layers[i],
                 input_shape = (nx,),
                 activation=activations[i],
                 kernel_regularizer=K.regularizers.l2(l2=lambtha)
             ))
         else:
             model.add(K.layers.Dense(
+                layers[i],
                 activation=activations[i],
                 kernel_regularizer=K.regularizers.l2(l2=lambtha)
             ))
