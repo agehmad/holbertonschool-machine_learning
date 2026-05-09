30bf3141c8e66accff26094c290b47242071aa33
Ahmad Ahadov <asusahmad26@gmail.com>
2026-05-09 16:41:44 +0400

    one hot

--- supervised_learning/keras/3-one_hot.py
+++ supervised_learning/keras/3-one_hot.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 """documented"""
 import tensorflow.keras as K
-import numpy as np
 
 
 def one_hot(labels, classes=None):
-    """documented"""
-    if classes == None:
-        classes = np.max(labels) + 1
-    return np.eye(classes)[labels]
+    if classes is None:
+        classes = max(labels) + 1
+    return K.utils.to_categorical(labels, num_classes=classes)
