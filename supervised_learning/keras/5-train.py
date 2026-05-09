f5493f9892502b4eb0a2a0fed063b52e40c333bb
Ahmad Ahadov <asusahmad26@gmail.com>
2026-05-09 17:03:21 +0400

    validation

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""documented"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size,
+                epochs, validation_data=None, verbose=True, shuffle=False):
+    """documented"""
+    if validation_data == None:
+        history = network.fit(
+            data,
+            labels,
+            batch_size=batch_size,
+            epochs=epochs,
+            verbose=verbose,
+            shuffle=shuffle
+        )
+    else:
+        history = network.fit(
+            data,
+            labels,
+            batch_size=batch_size,
+            epochs=epochs,
+            verbose=verbose,
+            shuffle=shuffle,
+            validation_data=validation_data
+        )
+    return history
