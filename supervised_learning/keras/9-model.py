adcf559ca7671280c7498da82d9b6bdaac1a65fb
Ahmad Ahadov <asusahmad26@gmail.com>
2026-05-12 18:56:36 +0400

    1

---
+++ supervised_learning/keras/9-model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""documented"""
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    """documented"""
+    network.save(filename)
+    return None
+
+def load_model(filename):
+    """documented"""
+    return K.models.load_model(filename)
